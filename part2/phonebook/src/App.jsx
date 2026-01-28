d029c704b47676fb3a2a958783e289489e0ab9ff
Artem Zhyrnyi <ic3ego@gmail.com>
2026-01-28 23:51:09 +0200

    solution for exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter/Filter";
 import PersonForm from "./components/PersonForm/PersonForm";
 import Persons from "./components/Persons/Persons";
@@ -8,6 +9,12 @@ const App = () => {
   const [query, setQuery] = useState("");
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(query.toLowerCase()),
