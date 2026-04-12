1005a8986f848474dc7a2ca2ae93038d9b02b6b2
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-12 10:00:46 +0300

    fo part 3 — task 3.11: 'serve static files from the backend'

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,13 +26,15 @@ let persons = [
 ];
 
 app.use(express.json());
+
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : " ";
 });
-
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
+
+app.use(express.static("dist"))
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
