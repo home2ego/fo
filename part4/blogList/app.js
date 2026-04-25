fcefcef219c763846743733cdb352fa9ba26fa8b
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-25 12:37:06 +0300

    fo part4: task 4.2 'blog list refactor'

---
+++ part4/blogList/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URL, { family: 4 })
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
