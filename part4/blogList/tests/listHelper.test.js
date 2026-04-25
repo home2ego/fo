d00a6ef00b69cf225ad67e24a4b5dc43bcee9a21
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-25 16:49:31 +0300

    fo part4: task 4.4 'helper functions and unit tests'

--- part4/blogList/tests/listHelper.test.js
+++
@@ -1,9 +0,0 @@
-const { test } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
-
-test('dummy returns one', () => {
-  const blogs = []
-
-  assert.strictEqual(listHelper.dummy(blogs), 1)
-})
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,3 +1,14 @@
 const dummy = (_blogs) => 1
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  switch (blogs.length) {
+    case 0:
+      return 0
+    case 1:
+      return blogs[0].likes
+    default:
+      return blogs.reduce((acc, item) => acc + item.likes, 0)
+  }
+}
+
+module.exports = { dummy, totalLikes }
