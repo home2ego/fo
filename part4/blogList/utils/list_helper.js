fb3f177ae17b4baf1084745981ae14e09750117e
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-26 10:22:36 +0300

    fo part4: task 4.6 'helper functions and unit tests'

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -10,4 +10,24 @@ const favoriteBlog = (array) => {
   return array.reduce((acc, current) => (current.likes > acc.likes ? current : acc))
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (array) => {
+  const authorBlogsSum = array.reduce((acc, current) => {
+    acc[current.author] = (acc[current.author] || 0) + 1
+
+    return acc
+  }, {})
+
+  let topAuthor = ''
+  let topCount = 0
+
+  for (const name in authorBlogsSum) {
+    if (authorBlogsSum[name] > topCount) {
+      topAuthor = name
+      topCount = authorBlogsSum[name]
+    }
+  }
+
+  return topAuthor ? { author: topAuthor, blogs: topCount } : {}
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
