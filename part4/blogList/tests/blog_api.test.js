8e5dc53fb701fae326dd1f3074a28b90a19b240f
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-28 18:37:03 +0300

    fo part4: task 4.10 'blog list tests refactor to use async/await'

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -43,24 +43,22 @@ test('blog fields are named id instead of _id', async () => {
   assert.deepStrictEqual(blogToView, response.body)
 })
 
-test('succeeds in creating a new blog post', () => {
+test('succeeds in creating a new blog post', async () => {
   const newBlog = { title: 'Cooking', author: 'Anna', url: 'http://bbc', likes: 12 }
 
-  return api
+  await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
-    .then(() => Blog.find({}))
-    .then((blogs) => {
-      const blogsAtEnd = blogs.map((b) => b.toJSON())
 
-      assert.deepStrictEqual(blogsAtEnd.length, initialBlogs.length + 1)
+  const blogs = await Blog.find({})
 
-      const titles = blogsAtEnd.map((b) => b.title)
+  const blogsAtEnd = blogs.map((b) => b.toJSON())
+  assert.deepStrictEqual(blogsAtEnd.length, initialBlogs.length + 1)
 
-      assert(titles.includes('Cooking'))
-    })
+  const titles = blogsAtEnd.map((b) => b.title)
+  assert(titles.includes('Cooking'))
 })
 
 after(async () => await mongoose.connection.close())
